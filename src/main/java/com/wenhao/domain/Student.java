4fb872e069fa31858ccea532023c6bf7847c4f30
wenhao <921790476@qq.com>
2016-09-13 15:02:02 +0800

    Rejected commit: Default

--- src/main/java/com/wenhao/domain/Student.java
+++ src/main/java/com/wenhao/domain/Student.java
@@ -4,8 +4,19 @@ package com.wenhao.domain;
  * Created by Administrator on 2016/9/13 0013.
  */
 public class Student {
+    private String id;
+
     private String name;
     private Integer age;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
